22d8fbf785a6298a687b2757f88862410a0c3c6d
Drumboyww <67783993+Drumboyww@users.noreply.github.com>
2022-01-11 09:11:12 +0100

    .

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
-// Set the date we're counting down to
-var endDate = new Date("Jan 17, 2022 00:00:00").getTime();
-// Update the count down every 1 second
-var x = setInterval(function() {
-    // Get todays date and time
-    var now = new Date().getTime();
-    // Find the distance between now an the count down date
-    var distance = endDate - now;
-    // Time calculations for days, hours, minutes and seconds
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    // Output the result in elements with id="cdHour/cdMin/cdSec"
-    document.getElementById("daysNum").innerHTML = ("0" + days).slice(-2);  
-    document.getElementById("hoursNum").innerHTML = ("0" + hours).slice(-2);
-    document.getElementById("minsNum").innerHTML = ("0" + minutes).slice(-2);
-    document.getElementById("secsNum").innerHTML = ("0" + seconds).slice(-2);
-    // If the count down is over, write some text 
-    if (distance < 0) {
-        clearInterval(x);
-        document.getElementById("countContainer").innerHTML = "EXPIRED";
-    }
-}, 1000);
+(function () {
+  const second = 1000,
+        minute = second * 60,
+        hour = minute * 60,
+        day = hour * 24;
+
+  //I'm adding this section so I don't have to keep updating this pen every year :-)
+  //remove this if you don't need it
+  let today = new Date(),
+      dd = String(today.getDate()).padStart(2, "0"),
+      mm = String(today.getMonth() + 1).padStart(2, "0"),
+      yyyy = today.getFullYear(),
+      nextYear = yyyy + 1,
+      dayMonth = "01/17/",
+      birthday = dayMonth + yyyy;
+  
+  today = mm + "/" + dd + "/" + yyyy;
+  if (today > birthday) {
+    birthday = dayMonth + nextYear;
+  }
+  //end
+  
+  const countDown = new Date(birthday).getTime(),
+      x = setInterval(function() {    
+
+        const now = new Date().getTime(),
